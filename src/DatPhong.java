47801488b7d8a251844732bf7443c6eabbc90b52
at30717 <23010217@st.phenikaa-uni.edu.vn>
2024-10-06 12:17:18 +0700

    DatPhong.java

--- src/DatPhong.java
+++ src/DatPhong.java
@@ -1,23 +1,21 @@
 public class DatPhong {
     private String maDat;
-    private String ngayDat;
-    private boolean trangThai; // true: đã đặt, false: chưa đặt
+    private boolean trangThai;
 
     public DatPhong(String maDat, String ngayDat, boolean trangThai) {
         this.maDat = maDat;
-        this.ngayDat = ngayDat;
         this.trangThai = trangThai;
     }
 
-    // Phương thức đặt phòng
-    public void datPhong() {
-        this.trangThai = true;
-        System.out.println("Phòng đã được đặt.");
+    public String getMaDat() {
+        return maDat;
     }
 
-    // Phương thức hủy đặt phòng
-    public void huyDatPhong() {
-        this.trangThai = false;
-        System.out.println("Phòng đã được hủy đặt.");
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
     }
 }
